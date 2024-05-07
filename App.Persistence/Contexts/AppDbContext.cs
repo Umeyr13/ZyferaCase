2dd458922902670827499e51f45f14d660c63dae
Umeyr13 <umeyr.gulcimen@gmail.com>
2024-05-07 16:16:39 +0300

    DesignedDbContext_AddedDbSet

--- App.Persistence/Contexts/AppDbContext.cs
+++ App.Persistence/Contexts/AppDbContext.cs
@@ -1,7 +1,22 @@
-﻿namespace ZyferaCase.App.Persistence.Contexts
+﻿using Microsoft.EntityFrameworkCore;
+using ZyferaCase.App.Domain.Entities;
+
+namespace ZyferaCase.App.Persistence.Contexts
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
