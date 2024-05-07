45545f7a968b122ee2e94763be8936137c87c6b2
Umeyr13 <umeyr.gulcimen@gmail.com>
2024-05-07 16:08:54 +0300

    CreatedEntities

---
+++ App.Domain/Entities/Grade.cs
@@ -0,0 +1,9 @@
+﻿namespace ZyferaCase.App.Domain.Entities
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ App.Domain/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ZyferaCase.App.Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string StdNumber { get; set; }
+        public List<Grade> Grades { get; set; }
+    }
+}
